75ea75b27db3a935b46c4aad91213cc3453fe145
Piyachai2310 <tartar0085@gmail.com>
2024-02-16 23:42:28 +0700

    Login

--- React/login/src/Login.js
+++ React/login/src/Login.js
@@ -1,104 +1,57 @@
-// import { useState } from "react";
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-// import {Form,Row,Col,Button} from 'react-bootstrap';
-// import { useNavigate } from "react-router-dom";
-
-// export default function Login(){
-//   const [validated, setValidated]=useState(false);
-//   const [username, setUsername]=useState("");
-//   const [password, setPassword]=useState("");
-//   let navigate = useNavigate();
-//   const onLogin = (event) => {
-//     const form = event.currentTarget;
-//     event.preventDefault();
-//     if (form.checkValidity()===false){
-//       event.stopPropagation();
-//     } else {
-//         doLogin();
-//     }
-//     setValidated(true);
-//   }
-
-//   const doLogin = async () => {
-//     // ตรวจสอบว่า username และ password ถูกต้องหรือไม่
-//     if (username === "admin" && password === "12345") {
-//       // ถ้าถูกต้อง, เปลี่ยนหน้าไปที่ "home"
-//       navigate("home", { replace: false });
-//     } else {
-//       // ถ้าไม่ถูกต้อง, แจ้งเตือน
-//       alert("Invalid Username/Password")
-//     }
-//   }
-
-//   return (
-//     <div className="container m-auto">
-//       <Form noValidate validated={validated} onSubmit={onLogin}>
-//         <Row className="mb-3">
-//           <Form.Group as={Col} controlId="validateUsername">
-//             <Form.Label> Username</Form.Label>
-//             <Form.Control
-//               required
-//               type="text"
-//               placeholder="Enter Username"
-//               onChange={(e) => setUsername(e.target.value)}
-//             />
-//             <Form.Control.Feedback type="invalid">
-//               กรุณากรอก Username
-//             </Form.Control.Feedback>
-//           </Form.Group>
-//         </Row>
-//         <Row className="mb-3">
-//           <Form.Group as={Col} controlId="validatePassword">
-//             <Form.Label> Password</Form.Label>
-//             <Form.Control
-//               required
-//               type="password"
-//               placeholder="Password"
-//               onChange={(e) => setPassword(e.target.value)}
-//             />
-//             <Form.Control.Feedback type="invalid">
-//               กรุณากรอก Password
-//             </Form.Control.Feedback>
-//           </Form.Group>
-//         </Row>
-//         <Row>
-//           <Col md={3}>
-//             <Button type="submit"> Login </Button>
-//           </Col>
-//         </Row>
-//       </Form>
-//     </div>
-//   );
-// }
-
 import { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Form, Row, Col, Button } from 'react-bootstrap';
+import {Form,Row,Col,Button} from 'react-bootstrap';
 import { useNavigate } from "react-router-dom";
 
-var md5 = require("md5");
 
-export default function Login() {
-  const [validated, setValidated] = useState(false);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  let navigate = useNavigate();
-
+// const md5 = require("md5");
+const md5 = require('blueimp-md5');
+export default function Login(){
+  const [validated, setValidated]=useState(false);
+  const [username, setUsername]=useState("");
+  const [password, setPassword]=useState("");
+  const navigate = useNavigate();
+  
   const onLogin = (event) => {
     const form = event.currentTarget;
     event.preventDefault();
-    if (form.checkValidity() === false) {
+    if (form.checkValidity()===false){
       event.stopPropagation();
     } else {
-      doLogin();
+        doLogin();
     }
     setValidated(true);
   }
 
+  // const doLogin = async() =>{
+  //   const response = await fetch(
+  //     "http://localhost:8080/login",
+  //     {
+  //       method:"POST",
+  //       headers: {
+  //         Accept:"application/json",
+  //         'Content-Type':'application/json',
+  //       },
+  //       body: JSON.stringify({
+  //         username:username,
+  //         password:password
+  //       })
+  //     }
+  //   );
+  //   const data = await response.json();
+  //   console.log(data);
+  //   if (data.result){
+  //     navigate("home",{replace:false});
+  //   }else
+  //   {
+  //     alert("Invalid Username/Password")
+  //   }
+  // }
+
+
+
   const getAuthenToken = async () => {
-    try {
       const response = await fetch(
         "http://localhost:8080/api/authen_request",
         {
@@ -108,69 +61,62 @@ export default function Login() {
             'Content-Type': 'application/json',
           },
           body: JSON.stringify({
-            username: username
-          }),
-          // mode: 'cors',
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error(`HTTP error: ${response.status}`);
-      }
-
-      const data = await response.json();
-      console.log(data);
-      return data;
-    } catch (error) {
-      console.error("Error fetching authen token:", error);
-      throw error;
-    }
-  };
-
-  const getAccessToken = async (authToken) => {
-    console.log(authToken);
-    try {
-      var baseString = username + "&" + md5(password);
-      var authenSignature = baseString;
-      console.log(baseString);
-      const response = await fetch(
-        "http://localhost:8080/api/access_request",
-        {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            auth_signature: authenSignature,
-            auth_token: authToken
+            username: md5(username)
           })
         }
       );
-
+  
+      if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+      console.log(md5(username));
       const data = await response.json();
       return data;
-    } catch (error) {
-      console.error("Error fetching access token:", error);
-      throw error;
-    }
+  };
+
+  const getAccessToken = async (authToken) =>{
+    var md5password = md5(password);
+    var baseString = username + "&" + md5password;
+    var authenSignature = md5(baseString);
+    
+    console.log(password);
+    console.log(md5password);
+    console.log(md5(username));
+    console.log(baseString);
+    console.log(authenSignature);
+    console.log(`authToken ${authToken}`);
+
+    const response = await fetch(
+      "http://localhost:8080/api/access_request",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+          auth_signature: authenSignature,
+          auth_token: authToken
+        })
+      }
+    );
+    console.log(authenSignature);
+    const data = await response.json();
+    return data;
   };
 
 
   const doLogin = async () => {
-    console.log("000");
-    console.log("000");
     try {
+      console.log("aa")
       const data1 = await getAuthenToken();
-      console.log("1");
+      console.log(data1)
       const authToken = data1.data.auth_token;
-      console.log("1.1");
+      console.log(authToken);
       const data2 = await getAccessToken(authToken);
-      console.log("2");
-      // console.log(access_token);
-      console.log(data1);
       console.log(data2);
-      // localStorage.setItem("access_token", data2.data.access_token);
+  
+      localStorage.setItem("access_token", data2.data.access_token);
       localStorage.setItem("user_id", data2.data.account_info.user_id);
       localStorage.setItem("username", username);
       localStorage.setItem("first_name", data2.data.account_info.first_name);
@@ -178,72 +124,56 @@ export default function Login() {
       localStorage.setItem("email", data2.data.account_info.email);
       localStorage.setItem("role_id", data2.data.account_info.role_id);
       localStorage.setItem("role_name", data2.data.account_info.role_name);
-      console.log("3");
-      navigate("home", { replace: false });
-
+  
+      navigate("home", {replace: false });
     } catch (error) {
-      console.error("Login failed:", error);
+      console.error("Error during login:", error);
       alert("Invalid Username/Password");
     }
-  }
+  };
 
-  // const doLogin = async () => {
-  //   const data1 = await getAuthenToken();
-  //   // const authToken = data1.data.auth_token;
-  //   const authToken = data1.data.auth_token;
-
-  //   const data2 = await getAccessToken(authToken);
-
-  //   localStorage.setItem("access_token", data2.data.access_token);
-  //   localStorage.setItem("user_id", data2.data.account_info.user_id);
-  //   localStorage.setItem("username", username);
-  //   // localStorage.setItem("password", password);
-  //   localStorage.setItem("first_name", data2.data.account_info.first_name);
-  //   localStorage.setItem("last_name", data2.data.account_info.last_name);
-  //   localStorage.setItem("email", data2.data.account_info.email);
-  //   localStorage.setItem("role_id", data2.data.account_info.role_id);
-  //   localStorage.setItem("role_name", data2.data.account_info.role_name);
-
-  //   navigate("home", { replace: false});
-  // }
-
-  return (
+  
+  return(
     <div className="container m-auto">
       <Form noValidate validated={validated} onSubmit={onLogin}>
         <Row className="mb-3">
-          <Form.Group as={Col} controlId="validateUsername">
+          <Form.Group as = {Col} controlId="validateUsername">
             <Form.Label> Username</Form.Label>
             <Form.Control
               required
               type="text"
               placeholder="Enter Username"
-              onChange={(e) => setUsername(e.target.value)}
+              onChange={(e)=> setUsername(e.target.value)}
             />
-            <Form.Control.Feedback type="invalid">
+            <Form.Control.Feedback type = "invalid">
               กรุณากรอก Username
             </Form.Control.Feedback>
           </Form.Group>
         </Row>
         <Row className="mb-3">
-          <Form.Group as={Col} controlId="validatePassword">
+          <Form.Group as = {Col} controlId="validatePassword">
             <Form.Label> Password</Form.Label>
             <Form.Control
               required
               type="password"
               placeholder="Password"
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={(e)=> setPassword(e.target.value)}
             />
-            <Form.Control.Feedback type="invalid">
+            <Form.Control.Feedback type = "invalid">
               กรุณากรอก Password
             </Form.Control.Feedback>
           </Form.Group>
         </Row>
         <Row>
           <Col md={3}>
-            <Button type="submit"> Login </Button>
+            <Button type = "submit"> Login </Button>
           </Col>
         </Row>
       </Form>
     </div>
+
   );
+
+
 }
+
--- server.js
+++ server.js
@@ -1,212 +1,165 @@
-// const http = require('http');
-
-// const hostname = '127.0.0.1';
-
-// const port = 3000;
-
-// const server = http.createServer((req,res) =>{
-//     res.statusCode = 200;
-//     res.setHeader("Content-Type","text/plain");
-//     res.end("Hello world uncle");
-// });
-
-// server.listen(port,hostname,() =>{
-//     console.log(`server running at http://${hostname}:${port}/`);
-// });
-
-// ctrl + C  เพื่อยกเลิก server
-
-//----------------------------------------------------------------------------------------------------------------------------------
-
-// const express = require('express');
-// const app = express();
-// const port = 3000;
-
-// var mysql = require('mysql');
-// var pool = mysql.createPool({
-//     connectionLimit: 10,
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'my-product'
-// });
-
-// app.get('/' , (req,res) => {
-//     res.send('Hello World by Express!')
-// }); 
-
-// app.get('/users' , (req,res) => {
-//     // res.send('Getting all user to you')
-//     pool.query("SELECT * FROM user" , function (error, results , fields) {
-//         if (error) throw error;
-
-//         res.json(results);
-//     });
-// }); 
-
-// app.get('/users/:userId' , (req,res) => {
-//     const userId = req.params.userId;
-
-//     pool.query("SELECT * FROM user WHERE user_id = ?", [userId], function (error, results , fields) {
-//         if (error) throw error;
-
-//         res.json(results);
-//     });
-// }); 
-
-
-// app.listen(port , () => {
-//     console.log(`Example app listening on port ${port}`)
-// });
-
-//----------------------------------------------------------------------------------------------------------------------------------
 const express = require('express');
 const app = express();
 const port = 8080;
+const bodyParser = require('body-parser');
+
+const jwt = require("jsonwebtoken");
+
 const cors = require("cors");
 app.use(cors());
 
-
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 var mysql = require('mysql');
 var pool = mysql.createPool({
-    connectionLimit: 10,
+    connectionLimit :10,
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: "",
     database: 'my-product'
 });
 
-app.get('/users', (req, res) => {
-    // res.send('Getting all user to you')
-    pool.query("SELECT * FROM users", function (error, results, fields) {
-        if (error) throw error;
+app.post('/add_user', (req, res) => {
+    const { id, name, old, weight, email } = req.body;
 
-        res.json(results);
-    });
+    pool.query("INSERT INTO users (id, name, old, weight, email) VALUES (?, ?, ?, ?, ?)",
+        [id, name, old, weight, email],
+        function(error, results, fields) {
+            if (error) {
+               if (error) throw error;
+            }
+
+            res.json(results);
+        }
+    );
 });
 
-app.get('/roles', (req, res) => {
-    // res.send('Getting all user to you')
-    pool.query("SELECT * FROM roles", function (error, results, fields) {
-        if (error) throw error;
 
-        res.json(results);
-    });
+    
+
+
+
+app.get ('/',(req, res) =>{
+    res.send('Hello');
 });
 
-const bodyParser = require('body-parser');
+app.get ('/users',(req, res) =>{
+   //res.send("Getting all users to you");
+   pool.query("select * from users",function(error, results,fields){
+    if (error) throw error;
 
+    res.json(results);
+   });
+});
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.listen(port, () =>{
+    console.log(`Example app listening on port ${port}`);
+});
 
-app.use(bodyParser.json());
-
-
-// app.post('/add_user', (req, res) => {
-//     const input = req.body;
-
-//     pool.query(
-//         "INSERT INTO users (user_id, user_name, user_pwd, first_name ,last_name, email) VALUES (?, ?, ?, ?, ? ,?)",
-//         [
-//             input.user_id,
-//             input.user_name,
-//             input.user_pwd,
-//             input.first_name,
-//             input.last_name,
-//             input.email
-//         ], function (error, results, fields) {
-//             if (error) throw error;
-
-//             res.json(results);
-//         });
-
-// });
-
-const jwt = require('jsonwebtoken'); // ต้อง import library jwt
+app.post("/login", (req, res) =>{
+    const username =req.body.username;
+    const password =req.body.password;
+    pool.query("SELECT * FROM users WHERE user_name = ? AND user_pwd = MD5(?)",[username,password],function (error,results,fields) {
+        if(error){
+            res.json({
+                result: false,
+                message : error.message
+            });
+        }
+    
+        
+        if(results.length){
+            res.json({
+                result : true
+            });
+        }
+        else{
+            res.json({
+                result: false,
+                message : "ไม่พบ username or password ไม่ถูกต้อง"
+            });
+        }
+    })
+});
 
 app.post("/api/authen_request", (req, res) => {
-    const sql = "SELECT * FROM users WHERE user_name = ?";
-    pool.query(sql, [req.body.username], (error, results) =>{
+    const sql = "SELECT * FROM users WHERE MD5(user_name) = ?";
+    pool.query(sql, [req.body.username], (error, results) => {
         var response;
-        if(error) {
-            console.log(response);
+        if (error) {
             response = {
                 result: false,
                 message: error.message
             };
-        }
-        else{
-            if (results.length){
+        } else {
+            if (results.length) {
+                console.log("start")
                 var payload = { username: req.body.username };
                 var secretKey = "MySecretKey";
+                console.log("start2")
                 const authToken = jwt.sign(payload, secretKey);
-                console.log("start test");
-                console.log("start test");
-                // res.json(authToken);
+                console.log("start3")
                 response = {
-                    result:true,
+                    result: true,
                     data: {
                         auth_token: authToken
                     }
                 };
-            }
-            else{
+            } else {
                 response = {
                     result: false,
                     message: "Username ไม่ถูกต้อง"
                 };
             }
         }
+        console.log(results);
         res.json(response);
+        
     });
 });
+
+
+
 
 app.post("/api/access_request", (req, res) => {
     const authenSignature = req.body.auth_signature;
     const authToken = req.body.auth_token;
-    // console.log("1");
-    // console.log(authToken);
-    try {
-        var decoded = jwt.verify(authToken, "MySecretKey");
-        // console.log(decoded);
-        if (decoded) {
-            const query = "SELECT a.user_id, a.user_name, a.user_pwd, a.first_name, a.last_name, a.email, a.role_id, b.role_name "
-                + "FROM users a JOIN roles b ON a.role_id = b.role_id WHERE MD5(CONCAT(user_name, '&', user_pwd)) = ?";
+
+    var decoded = jwt.verify(authToken, "MySecretKey");
+
+    if (decoded) {
+        const query = "SELECT a.user_id, a.user_name, a.first_name, a.last_name, a.email, a.role_id, b.role_name " +
+        "FROM users a JOIN roles b ON a.role_id = b.role_id WHERE MD5(CONCAT(user_name, '&', user_pwd)) = ?";
             pool.query(query, [authenSignature], (error, results) => {
-                var response;
+                var response ;
                 if (error) {
                     response = {
                         result: false,
                         message: error.message
                     };
-                    res.json(response);
                 } else {
                     if (results.length) {
                         var payload = {
-                            user_id: results[0].user_id,
-                            username: results[0].user_name,
+                            user_id: results[0].user_id, 
+                            user_name: results[0].user_name, 
                             first_name: results[0].first_name,
-                            last_name: results[0].last_name,
+                            last_name : results[0].last_name, 
                             email: results[0].email,
-                            role_id: results[0].role_id,
+                            role_id: results[0].role_id, 
                             role_name: results[0].role_name
                         };
                         const accessToken = jwt.sign(payload, "MySecretKey");
-                        response = { result: true, data: { access_token: accessToken, account_info: payload } };
-                        res.json(response);
+                        response = { result: true, data: { access_token: accessToken, account_info: payload}};
                     } else {
-                        response = { result: false, message: "Username หรือ Password ไม่ถูกต้อง" };
-                        res.json(response);
+                        response = { result: false, message: "Username หรือ Password ไม่ถูกต้อง"};
                     }
                 }
-            });
-        }
-    } catch (verifyError) {
-        res.json({ result: false, message: "ไม่สามารถตรวจสอบ Token ได้" });
-    }
-});
+                res.json(response);
 
-app.listen(port , () => {
-    console.log(`Example app listening on port ${port}`)
+                console.log(authenSignature);
+                console.log(results);
+                console.log(response);
+            });
+    }
 });
